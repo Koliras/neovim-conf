dd48a0fe2aa6c32aa688538a2c328ad074351eec
Koliras <gyrmanchyk230506@gmail.com>
2026-05-25 22:25:21 +0300

    odin formatting

--- lua/armouredgoose/plugins/lsp.lua
+++ lua/armouredgoose/plugins/lsp.lua
@@ -24,26 +24,26 @@ return {
 						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]definition")
 
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]definition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]symbols")
 
-					map("<leader>sm", require("telescope.builtin").man_pages, "[S]earch [M]anual")
+					map("<leader>sm", require("telescope.builtin").man_pages, "[S]earch [M]annual")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
+						"[W]orkspace [S]symbols"
 					)
 
 					-- Rename the variable under your cursor.
@@ -93,6 +93,7 @@ return {
 				ols = {
 					init_options = {
 						enable_references = true,
+						enable_format = true,
 						enable_hover = true,
 						enable_semantic_tokens = true,
 						enable_document_symbols = true,
@@ -172,7 +173,7 @@ return {
 				typescriptreact = { "biome", "prettierd", "prettier" },
 				javascript = { "biome", "prettierd", "prettier" },
 				javascriptreact = { "biome", "prettierd", "prettier" },
-				odin = { "ols" },
+				odin = { "odinfmt" },
 
 				["*"] = { "codespell" },
 			},
